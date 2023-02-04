9b44607086e5251aebf17ac67d31e937a1b2ea01
Dylan Vu <dylanvu9@gmail.com>
2023-02-03 21:38:21 -0800

    remove second listen

--- test-server/index.js
+++ test-server/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-const app = express()
+const app = express();
 const port = 5001;
 
 app.get('/', (req, res) => {
@@ -9,10 +9,6 @@ app.get('/', (req, res) => {
 app.post('/api', (req, res) => {
     console.log(req);
     res.send('POST request to the homepage')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
 })
 
 app.listen(port, () => {
