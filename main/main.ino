84f1190d4fe72ea6947c48c07e1546725f95fdbd
Dylan Vu <dylanvu9@gmail.com>
2023-02-04 20:23:17 -0800

    remove wifi capabilities

--- main/main.ino
+++ main/main.ino
@@ -1,77 +1,17 @@
+#include <vector>
 #include <WiFi.h>
-#include <HTTPClient.h>
+const int micPin = 13;
 
-// Your Domain name with URL path or IP address with path
-//String serverName = "https://2ec9-128-195-97-245.ngrok.io/";
-//String serverName = "https://www.google.com/";
-
-
-void createGetReq()
-{
-    Serial.print("Sending Request: \n");
-  // followed this tutorial: https://randomnerdtutorials.com/esp32-http-get-post-arduino/
-  HTTPClient http;
-
-  String serverPath = serverName;
-
-  http.begin(serverPath.c_str());
-
-  // add headers
-  http.addHeader("ngrok-skip-browser-warning", "123");
-
-  // Send HTTP GET request
-  int httpResponseCode = http.GET();
-
-  if (httpResponseCode > 0)
-  {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    String payload = http.getString();
-    Serial.println(payload);
-  }
-  else
-  {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  // Free resources
-  http.end();
-  return;
-}
-
-void wifiSetup()
-{
-  WiFi.mode(WIFI_STA);
-  const char *ssid = "Dylan-iPhone";
-  const char *password = "Goanteaters!";
-
-  Serial.print("ESP32 Mac Address: \n");
-  Serial.println(WiFi.macAddress());
-
-  WiFi.begin(ssid, password);
-  Serial.println("\nConnecting");
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(100);
-  }
-
-  Serial.println("\nConnected to the WiFi network");
-  Serial.print("Local ESP32 IP: ");
-  Serial.println(WiFi.localIP());
-}
-
-// <----- main arduino code ------>
-void setup()
-{
+void setup() {
+  // put your setup code here, to run once:
   Serial.begin(115200);
-  wifiSetup();
+
+  pinMode(micPin, INPUT);
 }
 
-void loop()
-{
-  createGetReq();
-  delay(1000);
+void loop() {
   // put your main code here, to run repeatedly:
+  const int soundVal = analogRead(micPin);
+  Serial.println(soundVal);
+  // Serial.write(soundVal);
 }
